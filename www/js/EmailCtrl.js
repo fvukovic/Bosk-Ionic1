3ff7471c6a92fe3c9a6aedd3e4a7ccf104894326
fvukovic <fvukovic@foi.hr>
2017-04-06 17:27:48 +0200

    napravljen email
    napravljen email

--- www/js/EmailCtrl.js
+++ www/js/EmailCtrl.js
@@ -1,5 +1,24 @@
 angular.module('Bosk')
-.controller('EmailCtrl', function($scope) {
+.controller('EmailCtrl', function($scope,$http) { 
+    $scope.email="";
+    $scope.message="";
+    
      
 
+     $scope.sendEmail=function(){  
+         $scope.email=document.getElementById("email").value;    
+         $scope.message=document.getElementById("message").value;
+           var request =$http({    
+             
+            method: "POST",
+            url: 'http://glutenfree.hr/rest/mail_sender.php',
+            data:{email:$scope.email,message:$scope.message},
+            headers : {'Content-Type' : 'application/x-www-form-urlencoded' }
+            
+        }); 
+        request.success(function (data) {
+            alert("Your email is sent!");
+        
+});
+     }
 });
